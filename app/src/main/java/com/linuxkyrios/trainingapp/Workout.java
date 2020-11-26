b07519fc1bb81e66575264206a7d200b50cb917e
Linux Kyrios <malinowskim92@protonmail.com>
2020-11-26 23:31:19 +0100

    Created workout class nad created fragment manager

--- app/src/main/java/com/linuxkyrios/trainingapp/Workout.java
+++ app/src/main/java/com/linuxkyrios/trainingapp/Workout.java
@@ -1,4 +1,36 @@
 package com.linuxkyrios.trainingapp;
 
 public class Workout {
+    private String name;
+    private String description;
+
+    public static final Workout[] workouts = {
+            new Workout("Rozciąganie kończyn",
+                    "5 pompek w staniu na rękach,\n10 przysiadów na ednej nodze,\n15 podciągnięć."),
+            new Workout("Ogólna agonia",
+                    "100 podciągnięć,\n100 pompek,\n100 brzuszków,\n100 przysiadów."),
+            new Workout("Tylko dla mięczaków",
+                    "5 podciągnięc,\n10 pompek,\n15 przysiadów."),
+            new Workout("Siła i dystans",
+                    "Bieg na 500 metrów,\n21 wydźwignięć ciężarka,\n21 podciągnięć."),
+            new Workout("Apokalipsa",
+                    "500 pompek,\n500 brzuszków,\n500 przysiadów,\n500 podciągnięć")
+    };
+
+    private Workout(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return this.name;
+    }
 }
--- app/src/main/java/com/linuxkyrios/trainingapp/WorkoutDetailFragment.java
+++ app/src/main/java/com/linuxkyrios/trainingapp/WorkoutDetailFragment.java
@@ -8,10 +8,16 @@ import android.view.ViewGroup;
 
 public class WorkoutDetailFragment extends Fragment {
 
+    private long workoutId;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         //Informing system, that this fragment uses layout placed in fragment_workout_detail
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    public void setWorkout(long id) {
+        this.workoutId = id;
     }
 }
