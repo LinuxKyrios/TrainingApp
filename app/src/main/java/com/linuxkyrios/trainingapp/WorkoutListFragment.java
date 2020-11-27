b656941d06e3f174d114b9ea617d4e2f558a7603
Linux Kyrios <malinowskim92@protonmail.com>
2020-11-27 01:17:36 +0100

    Updated WorkoutListFragment

--- app/src/main/java/com/linuxkyrios/trainingapp/WorkoutListFragment.java
+++ app/src/main/java/com/linuxkyrios/trainingapp/WorkoutListFragment.java
@@ -1,12 +1,11 @@
 package com.linuxkyrios.trainingapp;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.ListFragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 public class WorkoutListFragment extends ListFragment {
 
@@ -14,6 +13,18 @@ public class WorkoutListFragment extends ListFragment {
     public  View onCreateView(LayoutInflater inflater,
                               ViewGroup container,
                               Bundle savedInstanceState) {
+
+        String[] names = new String[Workout.workouts.length];
+        //Creating array with training names
+        for (int i = 0; i <names.length; i++) {
+            names[i] = Workout.workouts[i].getName();
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                //Uploading context from LayoutInflater object
+                inflater.getContext(), android.R.layout.simple_list_item_1,
+                names);
+        setListAdapter(adapter);
+
         //Using method onCreateView returns default ListFragment layout
         return super.onCreateView(inflater,
                 container, savedInstanceState);
