fbf3bd9548b323898fe807bb8a6e8f001d55769d
Linux Kyrios <malinowskim92@protonmail.com>
2020-12-01 00:26:49 +0100

    Created fragment transaction in Main Activity

--- app/src/main/java/com/linuxkyrios/trainingapp/MainActivity.java
+++ app/src/main/java/com/linuxkyrios/trainingapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.linuxkyrios.trainingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
@@ -15,8 +17,24 @@ public class MainActivity extends AppCompatActivity implements WorkoutListFragme
 
     @Override
     public void itemClicked(long id) {
-        Intent intent = new Intent(this, DetailActivity.class);
-        intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
-        startActivity(intent);
+        // Uploading reference to FrameLayout, which contains reference to WorkoutDetailFragment;
+        // This layout will be started only if app is started on big screen device.
+        View fragmentContainer = findViewById(R.id.fragment_container);
+        if (fragmentContainer != null) {
+            // Beginning of fragment transaction
+            WorkoutDetailFragment details = new WorkoutDetailFragment();
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            details.setWorkout(id);
+            // Replacing fragment
+            ft.replace(R.id.fragment_container, details);
+            // Setting animation for fading and opening new fragment
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            // Adding transaction to back stack
+            ft.addToBackStack(null);
+        } else {
+            Intent intent = new Intent(this, DetailActivity.class);
+            intent.putExtra(DetailActivity.EXTRA_WORKOUT_ID, (int)id);
+            startActivity(intent);
+        }
     }
 }
