1088506924bc416d3b0f852de69bd6fb8d1a8aba
Linux Kyrios <malinowskim92@protonmail.com>
2020-12-01 00:45:31 +0100

    Updated app with onSaveInstanceState method to save workoutId variable in case if device is rotated.

--- app/src/main/java/com/linuxkyrios/trainingapp/WorkoutDetailFragment.java
+++ app/src/main/java/com/linuxkyrios/trainingapp/WorkoutDetailFragment.java
@@ -12,6 +12,15 @@ public class WorkoutDetailFragment extends Fragment {
     private long workoutId;
 
     @Override
+    // Overriding onCreate method to save data if device is rotated
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            workoutId = savedInstanceState.getLong("workoutId");
+        }
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         //Informing system, that this fragment uses layout placed in fragment_workout_detail
@@ -29,6 +38,12 @@ public class WorkoutDetailFragment extends Fragment {
             TextView description = (TextView) view.findViewById(R.id.textDescription);
             description.setText(workout.getDescription());
         }
+    }
+
+    @Override
+    // This method saving workoutId variable in Bundle object called savedInstanceState
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putLong("workoutId", workoutId);
     }
 
     public void setWorkout(long id) {
