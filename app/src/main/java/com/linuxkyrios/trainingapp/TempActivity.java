bea568559f44d5482cca6d06c62953662c5d4934
Linux Kyrios <malinowskim92@protonmail.com>
2020-12-01 17:56:59 +0100

    Created content of TempActivity with transaction in case of device rotation

--- app/src/main/java/com/linuxkyrios/trainingapp/TempActivity.java
+++ app/src/main/java/com/linuxkyrios/trainingapp/TempActivity.java
@@ -1,7 +1,7 @@
 package com.linuxkyrios.trainingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 
 public class TempActivity extends AppCompatActivity {
@@ -10,5 +10,16 @@ public class TempActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_temp);
+        //Fragment should not be added if activity was restored
+        if (savedInstanceState == null) {
+            StopwatchFragment stopwatch = new StopwatchFragment();
+            //Beginning transaction
+            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+            ft.add(R.id.stopwatch_container, stopwatch); //Adding stopwatch to layout, and all transaction to back stack
+            ft.addToBackStack(null);
+            ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+            //Approve the transaction
+            ft.commit();
+        }
     }
 }
